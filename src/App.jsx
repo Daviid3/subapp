775c9dbd72f3b0f3eb104a8ee78175ba80bdb141
Abel Cherkos <abelcherkos@abels-air.mynetworksettings.com>
2024-11-07 19:39:10 -0500

    home cusomized

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Home from './components/Home'
+import './App.css'
+import Navbar from './components/NavBar'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    
+    <Home/>
+    {/* <Navbar/> */}
+    
+    
+    </>
+  )
+}
+
+export default App
